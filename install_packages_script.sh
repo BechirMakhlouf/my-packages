9397794dc86e3ab8a7b66d179406940f0f19edb1
Bechir Makhlouf <73342070+BechirMakhlouf@users.noreply.github.com>
2023-08-02 16:11:41 +0100

    script

---
+++ install_packages_script.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+CURR_USER="copernicus"
+
+PACMAN_PACKAGES_LIST=(
+  'git'
+  'sudo'
+  'base'
+  'base-devel'
+  'neofetch'
+  'chromium'
+  'firefox'
+  'obsidian'
+  'qbittorrent'
+  'tldr'
+  'man'
+  'waybar'
+  'tldr'
+  'zsh'
+  'nvim'
+  'yes'
+  'exa'
+  'bat'
+  'navi'
+  'thefuck'
+  'ranger'
+  'tmux'
+  'noto-fonts'
+  'lxappearance'
+  'discord'
+  'vlc'
+  'nodejs'
+  'go'
+  'docker'
+  'xorg-xwayland'
+  'exfat-utils'
+  'ntfs-3g'
+  'fuse'
+  'fuse3'
+  'grub'
+  'runuser'
+  'efibootmgr'
+  'os-prober'
+  'ssh'
+  'feh'
+  'fontconfig'
+  'pipewire'
+  'pipewire-pulse'
+  'wl-clipboard'
+  'jq'
+  'fzf'
+  'kitty'
+  'sway'
+  'swaybg'
+  'swaylock'
+  'pavucontrol'
+  'wofi'
+  'btop'
+  'htop'
+)
+
+AUR_PACKAGES_LIST=(
+  'ly'
+  'fastfetch'
+  'nerdfetch'
+  'spotify-adblock'
+  'nerd-fonts-inter'
+  'dracula-gtk-theme'
+  'ttf-iosevka-nerd'
+  'ttf-jetbrains-mono-nerd'
+  'ttf-ubuntu-arabic'
+)
+
+SERVICES_TO_ENABLE=(
+  'NetworkManager'
+  'ly'
+)
+pacman -Syyu --noconfirm
+
+for i in "${PACMAN_PACKAGES_LIST[@]}"; do
+  PACMAN_PACKAGES_STR="$PACMAN_PACKAGES_STR $i"
+done
+
+echo $PACMAN_PACKAGES_STR
+pacman --noconfirm -S $PACMAN_PACKAGES_STR
+
+# remove previous yay install folder if it exists
+rm -rf /tmp/install_yay/
+
+# install yay aur helper
+cd /tmp/ && mkdir install_yay && cd install_yay && pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si 
+
+for i in "${AUR_PACKAGES_LIST[@]}"; do
+   AUR_PACKAGES_STR="$AUR_PACKAGES_STR $i"
+done
+
+sudo -u $CURR_USER yay --answerdiff=None --noconfirm -S $AUR_PACKAGES_STR
+
+echo "you need to enable these Packages: "
+
+for i in "${SERVICES_TO_ENABLE[@]}"; do
+  echo "- $i"
+done
